28ff09ee9a795387330259b15f9f2bbbbd100b56
Its-rafx <rafael.1998eli@gmail.com>
2025-11-08 00:20:56 +0000

    Update exercise_filter_by_id.js

--- api/exercicio/exercise_filter_by_id.js
+++ api/exercicio/exercise_filter_by_id.js
@@ -1,17 +1,14 @@
-// Copie o código abaixo e cole no seu arquivo index.js
-
-
 // Criando uma nova rota que recebe um ID pela URL. `:monstro_id`
 app.get('/monstros/:monstro_id', (req, res) => {
 
-    // Descubra como pegar o ID passado pela URL através do atributo req.params 
-    let id = req.params._____;
-    // Pesquise sobre o método find em javascript e filtre o monstro por ID.
-    // Vale 15 pts
-    let monstro = ________________ ;
+    // Pegando o ID passado pela URL
+    let id = req.params.monstro_id;
+
+    // Filtrando o monstro pelo ID
+    let monstro = monstros.find(m => m.id == id);
 
     if (monstro) {
-        res.json(monstro)
+        res.json(monstro);
     } else {
         res.status(404).json({ erro: 'Nenhum monstro encontrado.' });
     }
